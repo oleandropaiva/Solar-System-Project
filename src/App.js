fb541241682f1204aeee9d8acaef0ef64d308c95
Leandro Paiva <leandroapaiva@yahoo.com>
2022-04-06 16:25:50 -0300

    iten 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <p>Sistema Solar</p>);
+      <Header />
+
+    );
   }
 }
 
